b8f249ffd88274bcbeaf78f30caeae09f1d201cb
WentseChen <cwz19@mails.tsinghua.edu.cn>
2024-03-22 10:12:01 -0400

    qmix

--- qmix.sh
+++ qmix.sh
@@ -1,3 +1,8 @@
-git add . 
-git commit -m "qmix"
-CUDA_VISIBLE_DEVICES=2 python src/main.py --config=qmix --env-config=sc2_gen_protoss
+# git add . 
+# git commit -m "qmix"
+
+# SMAC-v2
+# CUDA_VISIBLE_DEVICES=3 python src/main.py --config=qmix --env-config=sc2_gen_zerg 
+
+# SMAC-v1
+CUDA_VISIBLE_DEVICES=3 python src/main.py --config=qmix --env-config=sc2 with env_args.map_name=3s5z_vs_3s6z
--- src/modules/mixers/nmix.py
+++ src/modules/mixers/nmix.py
@@ -49,9 +49,19 @@ class Mixer(nn.Module):
         self.hyper_w5 = nn.Sequential(
             nn.Linear(self.state_dim, args.hypernet_embed),
             nn.ReLU(inplace=True),
-            nn.Linear(args.hypernet_embed, self.n_agents)
+            nn.Linear(args.hypernet_embed, self.n_agents * self.embed_dim//2)
         )
         self.hyper_b5 = nn.Sequential(
+            nn.Linear(self.state_dim, args.hypernet_embed),
+            nn.ReLU(inplace=True),
+            nn.Linear(args.hypernet_embed, self.n_agents * self.embed_dim//2)
+        )
+        self.hyper_w6 = nn.Sequential(
+            nn.Linear(self.state_dim, args.hypernet_embed),
+            nn.ReLU(inplace=True),
+            nn.Linear(args.hypernet_embed, self.n_agents * self.embed_dim//2)
+        )
+        self.hyper_b6 = nn.Sequential(
             nn.Linear(self.state_dim, args.hypernet_embed),
             nn.ReLU(inplace=True),
             nn.Linear(args.hypernet_embed, self.n_agents)
@@ -65,18 +75,24 @@ class Mixer(nn.Module):
         if qval_shape[-2] == self.n_agents:
             self.dim_idx = -3
             qvals = qvals.reshape(-1, self.n_agents, qvals.shape[-1])
-            w = self.hyper_w5(states).view(-1, self.n_agents, 1)
-            b = self.hyper_b5(states).view(-1, self.n_agents, 1)
+            w1 = self.hyper_w5(states).view(-1, self.embed_dim//2, self.n_agents, 1)
+            b1 = self.hyper_b5(states).view(-1, self.embed_dim//2, self.n_agents, 1)
+            w2 = self.hyper_w6(states).view(-1, self.embed_dim//2, self.n_agents, 1)
+            b2 = self.hyper_b6(states).view(-1, self.embed_dim//2, self.n_agents, 1)
         if qval_shape[-1] == self.n_agents:
             self.dim_idx = -2
             qvals = qvals.reshape(-1, self.n_agents)
-            w = self.hyper_w5(states).view(-1, self.n_agents)
-            b = self.hyper_b5(states).view(-1, self.n_agents)
+            w1 = self.hyper_w5(states).view(-1, self.embed_dim//2, self.n_agents)
+            b1 = self.hyper_b5(states).view(-1, self.embed_dim//2, self.n_agents)
+            w2 = self.hyper_w6(states).view(-1, self.embed_dim//2, self.n_agents)
+            b2 = self.hyper_b6(states).view(-1, self.embed_dim//2, self.n_agents)
             
         if self.abs:
-            w = w.abs()
+            w1 = w1.abs()
+            w2 = w2.abs()
         
-        y = qvals * w + b 
+        y = F.elu(qvals * w1 + b1) 
+        y = (y * w2).sum(dim=-3, keepdim=True) + b2
         
         return y.reshape(qval_shape)
     
@@ -97,7 +113,7 @@ class Mixer(nn.Module):
         
         y = F.elu(qvals * w1 + b1)
         y = (y * w2).sum(dim=-3, keepdim=True) + b2
-        y = y + qvals
+        # y = y + qvals
         
         return y.reshape(qval_shape)
 
