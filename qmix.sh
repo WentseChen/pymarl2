d5d7b5246ef3a260fbdd2a80e6520bf8de411275
WentseChen <cwz19@mails.tsinghua.edu.cn>
2024-03-22 14:01:16 -0400

    qmix

--- qmix.sh
+++ qmix.sh
@@ -5,4 +5,4 @@ git commit -m "qmix"
 # CUDA_VISIBLE_DEVICES=3 python src/main.py --config=qmix --env-config=sc2_gen_zerg 
 
 # SMAC-v1
-CUDA_VISIBLE_DEVICES=0 python src/main.py --config=qmix --env-config=sc2 with env_args.map_name=2c_vs_64zg
+CUDA_VISIBLE_DEVICES=1 python src/main.py --config=qmix --env-config=sc2 with env_args.map_name=2c_vs_64zg
