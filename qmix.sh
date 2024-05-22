f0b910009cde5a360864a8f8f510c9dcba8ffb69
WentseChen <cwz19@mails.tsinghua.edu.cn>
2024-05-22 01:50:59 -0400

    update

--- qmix.sh
+++ qmix.sh
@@ -1,3 +1,3 @@
-git add .
-git commit -m "update"
-CUDA_VISIBLE_DEVICES=0 python src/main.py --config=qmix --env-config=sc2_gen_terran
+# git add .
+# git commit -m "update"
+CUDA_VISIBLE_DEVICES=0 python src/main.py --config=qmix --env-config=mpe
