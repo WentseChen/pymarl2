eb08b07b38493f173dd7bdd8625e41d6713ecca8
WentseChen <cwz19@mails.tsinghua.edu.cn>
2024-06-19 15:08:52 -0400

    update

--- src/envs/mpe/scenarios/simple_spread.py
+++ src/envs/mpe/scenarios/simple_spread.py
@@ -53,11 +53,11 @@ class Scenario(BaseScenario):
 
         # set random initial states
         for agent in world.agents:
-            agent.state.p_pos = np_random.uniform(-1, +1, world.dim_p) * 2.
+            agent.state.p_pos = np_random.uniform(-1, +1, world.dim_p) * 1.
             agent.state.p_vel = np.zeros(world.dim_p)
             agent.state.c = np.zeros(world.dim_c)
         for i, landmark in enumerate(world.landmarks):
-            landmark.state.p_pos = 0.8 * np_random.uniform(-1, +1, world.dim_p) * 2.
+            landmark.state.p_pos = 0.8 * np_random.uniform(-1, +1, world.dim_p) * 1.
             landmark.state.p_vel = np.zeros(world.dim_p)
 
     def benchmark_data(self, agent, world):
